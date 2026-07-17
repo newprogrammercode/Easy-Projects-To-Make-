c7f556dc50ac4a71a63d6440c50961f4052ecedf
Resid <yusubluresid@gmail.com>
2026-07-17 20:07:04 +0400

    Fixed everything.New update soon!

--- basic_encrypter/main.py
+++ basic_encrypter/main.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from cryptography.fernet import Fernet
 import os
-
+import base64
 class Main:
+
+    
     def __init__(self):
         self.root = Tk()
         self.root.title("Encoder")
@@ -13,14 +15,14 @@ class Main:
         self.original_text = Text(self.root , font = ("Comic Sans MS" , 12 , "bold") , fg = "lime" , padx = 2 , pady = 2, bg = "white", height = 10)
         self.original_text.pack(fill = "x")
         
-       
-        self.original_text.bind("<Return>", lambda event: self.main("the_key_of_the_proggram.key"))
+        self.decrypt_but = Button(self.root , font = ("Comic Sans MS" , 12, "bold") , fg = "red" , borderwidth=0,bd=0 , command= lambda: self.decrypt())
+        self.decrypt_but.place(relx = 0.5 , rely = 0.5)
         
-        self.button = Button(self.root , text="Encrypt", font = ("Comic Sans MS" ,  12 , "bold") , fg = "red" , padx = 2 , pady = 2,borderwidth= 0 , bd = 0 , command=lambda: self.main("the_key_of_the_proggram.key"))
+        self.button = Button(self.root , text="Encrypt", font = ("Comic Sans MS" ,  12 , "bold") , fg = "red" , padx = 2 , pady = 2,borderwidth= 0 , bd = 0 , command=lambda: self.encrypt("the_key_of_the_proggram.key"))
         self.button.pack(pady = 10)
         self.encrypted_text = Text(self.root , font = ("Comic Sans MS" , 12 , "bold") , fg = "lime" , pady = 2, padx = 2 , bg = "white", height = 10)
         self.encrypted_text.pack(fill = "x")
-    def main(self , file):
+    def encrypt(self , file):
         def load_key(file):
             if os.path.exists(file):
                 with open(file , "rb") as f:
